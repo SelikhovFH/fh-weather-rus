dee5cadaeac815bd5cbc9e698f7b8bb80f6b27d5
RuslanTkachenko <ruslan.tkachenko.dev@gmail.com>
2022-10-04 17:43:08 +0300

    CRA init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-disable */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/*eslint-enable */
