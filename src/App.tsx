0bae3697739f348ab591716a8159602f6a0e42af
RuslanTkachenko <ruslan.tkachenko.dev@gmail.com>
2022-10-05 17:25:05 +0300

    city-page jsx without styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import CityPage from "./components/detailed-city-page/CityPage";
 import LoginPage from './components/login/LoginPage';
 import WeatherPage from './components/weather-page/WeatherPage';
 
 const App = () => {
   return (
-    <div>
+    <>
       {/* <LoginPage/> */}
-      <WeatherPage/>
-    </div>
+      {/* <WeatherPage/> */}
+      <CityPage/>
+    </>
   );
 };
 
